16dba3068b68d00dcb84255b421e69382e564adc
Kevims <kcodeyt@gmail.com>
2022-06-20 11:23:15 +0200

    Update 0.0.7-SNAPSHOT: Add local ip to config and some improvements

---
+++ src/main/java/de/kcodeyt/vanilla/util/SimpleMath.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2022 KCodeYT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.kcodeyt.vanilla.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class SimpleMath {
+
+    public int ceil(float floatNumber) {
+        final int truncated = (int) floatNumber;
+        return floatNumber > truncated ? truncated + 1 : truncated;
+    }
+
+}
