8130fe69026c34f111e839e46cac0b0a8b8ecaa3
Kevims <kcodeyt@gmail.com>
2022-07-14 17:19:02 +0200

    Update 0.0.8-SNAPSHOT: Update to 1.19.10 (#17)

--- src/main/java/de/kcodeyt/vanilla/command/LocateCommand.java
+++ src/main/java/de/kcodeyt/vanilla/command/LocateCommand.java
@@ -38,8 +38,19 @@ public class LocateCommand extends Command {
         super("locate", "Locates a specific structure");
         this.setPermission("vanilla.command.locate");
         this.commandParameters.clear();
-        this.commandParameters.put("LocateArgs", new CommandParameter[]{
-                CommandParameter.newEnum("feature", false, new CommandEnum("Feature", StructureFeatureType.names()))
+        this.commandParameters.put("LocateArgs0", new CommandParameter[]{
+                CommandParameter.newEnum("feature", false, new CommandEnum("Structure", LocateSubcommandStructureEnum.names())),
+                CommandParameter.newEnum("useNewChunksOnly", true, new CommandEnum("Boolean", "true", "false"))
+        });
+        this.commandParameters.put("LocateArgs1", new CommandParameter[]{
+                CommandParameter.newEnum("subcommand", false, new CommandEnum("LocateSubcommandStructure", "structure")),
+                CommandParameter.newEnum("feature", false, new CommandEnum("Structure", LocateSubcommandStructureEnum.names())),
+                CommandParameter.newEnum("useNewChunksOnly", true, new CommandEnum("Boolean", "true", "false"))
+        });
+        this.commandParameters.put("LocateArgs2", new CommandParameter[]{
+                CommandParameter.newEnum("subcommand", false, new CommandEnum("LocateSubcommandBiome", "biome")),
+                CommandParameter.newEnum("feature", false, new CommandEnum("Structure", SubcommandBiomeEnum.names())),
+                CommandParameter.newEnum("useNewChunksOnly", true, new CommandEnum("Boolean", "true", "false"))
         });
     }
 
@@ -76,8 +87,7 @@ public class LocateCommand extends Command {
     }
 
     @SuppressWarnings("SpellCheckingInspection")
-    private enum StructureFeatureType {
-
+    private enum LocateSubcommandStructureEnum {
         ANCIENTCITY,
         BASTIONREMNANT,
         BURIEDTREASURE,
@@ -86,22 +96,116 @@ public class LocateCommand extends Command {
         MANSION,
         MINESHAFT,
         MONUMENT,
-        RUINS,
         PILLAGEROUTPOST,
         RUINEDPORTAL,
+        RUINS,
         SHIPWRECK,
         STRONGHOLD,
         TEMPLE,
         VILLAGE;
 
         static String[] names() {
-            final StructureFeatureType[] values = values();
+            final LocateSubcommandStructureEnum[] values = values();
             final String[] names = new String[values.length];
             for(int i = 0; i < values.length; i++)
                 names[i] = values[i].name().toLowerCase();
             return names;
         }
+    }
 
+    private enum SubcommandBiomeEnum {
+        BAMBOO_JUNGLE,
+        BAMBOO_JUNGLE_HILLS,
+        BASALT_DELTAS,
+        BEACH,
+        BIRCH_FOREST,
+        BIRCH_FOREST_HILLS,
+        BIRCH_FOREST_HILLS_MUTATED,
+        BIRCH_FOREST_MUTATED,
+        COLD_BEACH,
+        COLD_OCEAN,
+        COLD_TAIGA,
+        COLD_TAIGA_HILLS,
+        COLD_TAIGA_MUTATED,
+        CRIMSON_FOREST,
+        DEEP_COLD_OCEAN,
+        DEEP_DARK,
+        DEEP_FROZEN_OCEAN,
+        DEEP_LUKEWARM_OCEAN,
+        DEEP_OCEAN,
+        DESERT,
+        DESERT_HILLS,
+        DESERT_MUTATED,
+        DRIPSTONE_CAVES,
+        EXTREME_HILLS,
+        EXTREME_HILLS_EDGE,
+        EXTREME_HILLS_MUTATED,
+        EXTREME_HILLS_PLUS_TREES,
+        EXTREME_HILLS_PLUS_TREES_MUTATED,
+        FLOWER_FOREST,
+        FOREST,
+        FOREST_HILLS,
+        FROZEN_OCEAN,
+        FROZEN_PEAKS,
+        FROZEN_RIVER,
+        GROVE,
+        HELL,
+        ICE_MOUNTAINS,
+        ICE_PLAINS,
+        ICE_PLAINS_SPIKES,
+        JAGGED_PEAKS,
+        JUNGLE,
+        JUNGLE_EDGE,
+        JUNGLE_EDGE_MUTATED,
+        JUNGLE_HILLS,
+        JUNGLE_MUTATED,
+        LEGACY_FROZEN_OCEAN,
+        LUKEWARM_OCEAN,
+        LUSH_CAVES,
+        MANGROVE_SWAMP,
+        MEADOW,
+        MEGA_TAIGA,
+        MEGA_TAIGA_HILLS,
+        MESA,
+        MESA_BRYCE,
+        MESA_PLATEAU,
+        MESA_PLATEAU_MUTATED,
+        MESA_PLATEAU_STONE,
+        MESA_PLATEAU_STONE_MUTATED,
+        MUSHROOM_ISLAND,
+        MUSHROOM_ISLAND_SHORE,
+        OCEAN,
+        PLAINS,
+        REDWOOD_TAIGA_HILLS_MUTATED,
+        REDWOOD_TAIGA_MUTATED,
+        RIVER,
+        ROOFED_FOREST,
+        ROOFED_FOREST_MUTATED,
+        SAVANNA,
+        SAVANNA_MUTATED,
+        SAVANNA_PLATEAU,
+        SAVANNA_PLATEAU_MUTATED,
+        SNOWY_SLOPES,
+        SOULSAND_VALLEY,
+        STONE_BEACH,
+        STONY_PEAKS,
+        SUNFLOWER_PLAINS,
+        SWAMPLAND,
+        SWAMPLAND_MUTATED,
+        TAIGA,
+        TAIGA_HILLS,
+        TAIGA_MUTATED,
+        THE_END,
+        WARM_OCEAN,
+        WARPED_FOREST;
+
+        static String[] names() {
+            final SubcommandBiomeEnum[] values = values();
+            final String[] names = new String[values.length];
+            for(int i = 0; i < values.length; i++)
+                names[i] = values[i].name().toLowerCase();
+            return names;
+        }
     }
 
 }
