c383a786510abb261931ddd230e9ba413e0de1d7
Kevims <kcodeyt@gmail.com>
2022-11-05 10:21:37 +0100

    Update bedrock dedicated server to 1.19.41.01

--- src/main/java/de/kcodeyt/vanilla/generator/server/BedrockDedicatedServer.java
+++ src/main/java/de/kcodeyt/vanilla/generator/server/BedrockDedicatedServer.java
@@ -38,8 +38,8 @@ import java.util.zip.ZipInputStream;
  */
 class BedrockDedicatedServer {
 
-    private static final String WINDOWS_DIST = "https://minecraft.azureedge.net/bin-win/bedrock-server-1.19.40.02.zip";
-    private static final String LINUX_DIST = "https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.40.02.zip";
+    private static final String WINDOWS_DIST = "https://minecraft.azureedge.net/bin-win/bedrock-server-1.19.41.01.zip";
+    private static final String LINUX_DIST = "https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.41.01.zip";
 
     private static final File TEMP_DIRECTORY = new File("temp");
     private static final CompletableFuture<File> FUTURE = new CompletableFuture<>();
